c83bcf1d86decb4f6725f10b53a987a583737b84
BreslinIT <pokemax209@gmail.com>
2023-04-05 16:07:38 -0700

    last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame, sys, time
 from pygame.locals import QUIT
-from myBrain import myBrain
 import random, math, json
 
 class Brain:
@@ -9,6 +8,25 @@ class Brain:
         Given a gamestate, return a tuple of ((x, y), (x, y)) to move the player and shoot a bullet.
         '''
         raise NotImplemented
+    
+class myBrain(Brain):
+    def __init__(self):
+        super().__init__()
+
+    def getAction(self, timestep: int, area: tuple, playerSize: tuple, myPosition: tuple, myVelocity: tuple, myBullets: list, enemyPosition: tuple, enemyVelocity: tuple, enemyBullets: list, blockSize: tuple, blockPosition: tuple, blockVelocity: tuple) -> tuple:
+        '''
+        Given a gamestate, return a tuple of ((x, y), (x, y)) to move the player and shoot a bullet.
+        x and y must each be either -1, 0, or 1.
+        '''
+        choices = [-1, 0, 1]
+
+        moveX: int = random.choice(choices)
+        moveY: int = random.choice(choices)
+        shootX: int = random.choice(choices)
+        shootY: int = random.choice(choices)
+
+        return ((moveX, moveY), (shootX, shootY))
+
 
 class dumbBrain(Brain):
     def __init__(self):
